165644271fe5c8be046e8374ed0530bee8415e32
Jithuraj <contact.jithuraj@gmail.com>
2023-05-23 23:35:19 +0530

    improvement

--- lib/package_sample.dart
+++ lib/package_sample.dart
@@ -1,7 +1,13 @@
 library package_sample;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+  }
 }
